8c1c8821da378d568a06687d07eecfe8f914fc21
Ignacio Arranz <ignacio.a.oviedo@gmail.com>
2020-09-30 15:40:59 +0200

    yes grid

--- src/test/java/automation/saucedemo/stepDefinintions/Hook.java
+++ src/test/java/automation/saucedemo/stepDefinintions/Hook.java
@@ -1,8 +1,10 @@
 package automation.saucedemo.stepDefinintions;
 
+import automation.saucedemo.config.Configuration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -20,13 +22,34 @@ public class Hook {
     }
 
     @Before
-    public static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        //WebDriverManager.firefoxdriver().setup();
-        //driver = new FirefoxDriver();
-        //driver.manage().window().maximize();
+    public static void setupClass() throws Exception {
+        Configuration config = Configuration.getConfiguration();
+        String browser = config.getBrowserType();
+        driver = null;
+
+        if (config.getIsGridRun()) {
+            //driver = RemoteBrowserSet.getRemoteWebDriver();
+        } else {
+            if (browser.equals("chrome")) {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                driver.manage().window().maximize();
+
+            } else if (browser.equals("firefox")) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                driver.manage().window().maximize();
+
+            } else if (browser.equals("internetexplorer")) {
+                //TODO internetexplorer is not implemented yet
+//                WebDriverManager.iedriver().setup();
+//                driver = new InternetExplorerDriver();
+//                driver.manage().window().maximize();
+
+            } else {
+                throw new Exception();
+            }
+        }
     }
 
     @After
